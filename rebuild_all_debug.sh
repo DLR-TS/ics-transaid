c9b412d319c5f352d18022c8be410ed135af4285
Leonhard Lücken <leonhard.luecken@dlr.de>
2019-01-31 15:13:59 +0200

    Update rebuild_all_debug.sh

--- rebuild_all_debug.sh
+++ rebuild_all_debug.sh
@@ -6,13 +6,13 @@
 cd sumo/build/cmake-build
 echo "## Rebuilding sumo in $PWD..."
 cmake ../.. -DSUMO_UTILS=TRUE -DCMAKE_INSTALL_PREFIX=$PWD/../../.. -DCMAKE_BUILD_TYPE=Debug
-make install -j
+make install -j8
 cd ../../..
 
 cd ns-3.20
 echo "## Rebuilding ns-3 in $PWD..."
 ./waf configure --enable-debug --prefix=$PWD/..
-./waf -j
+./waf -j8
 ./waf install
 cd ..
 
@@ -20,32 +20,32 @@ cd iCS
 echo "## Rebuilding iCS in $PWD..."
 autoreconf -i
 ./configure --enable-debug --prefix=$PWD/..
-make install -j
+make install -j8
 cd ..
 
 cd iTETRIS-Applications/traffic-monitor
 echo "## Rebuilding demoApp in $PWD..."
 autoreconf -i
 ./configure --enable-debug --prefix=$PWD/../..
-make install -j
+make install -j8
 cd ../..
 
 cd iTETRIS-Applications/baseApp
 echo "## Rebuilding baseApp in $PWD..."
 autoreconf -i
 ./configure --enable-debug
-make -j
+make -j8
 
 cd ../testApp
 echo "## Rebuilding testApp in $PWD..."
 autoreconf -i
 ./configure --enable-debug --prefix=$PWD/../..
-make install -j
+make install -j8
 cd ../..
 
 cd LightCommSimulator
 echo "## Rebuilding LightComm in $PWD..."
 autoreconf -i
 ./configure --enable-debug --prefix=$PWD/..
-make install -j
+make install -j8
 cd ..
