24712ad051539d535f3a19e3263e7b37efb116d6
Leonhard Lücken <leonhard.luecken@dlr.de>
2019-01-31 15:12:52 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -6,13 +6,13 @@ export SUMO_HOME=$PWD/share/sumo
 mkdir sumo/build/cmake-build
 cd sumo/build/cmake-build
 cmake ../.. -DSUMO_UTILS=TRUE -DCMAKE_INSTALL_PREFIX=$PWD/../../..
-make install -j
+make install -j8
 cd ../../..
 
 echo "## Setting up ns-3..."
 cd ns-3.20
 ./waf configure --prefix=$PWD/..
-./waf -j
+./waf -j8
 ./waf install
 cd ..
 
@@ -20,14 +20,14 @@ cd iCS
 echo "## Setting up iCS..."
 autoreconf -i
 ./configure --prefix=$PWD/..
-make install -j
+make install -j8
 cd ..
 
 echo "## Setting up demoApp..."
 cd iTETRIS-Applications/traffic-monitor
 autoreconf -i
 ./configure --prefix=$PWD/../..
-make install -j
+make install -j8
 cd ../..
 
 echo "## Setting up baseApp..."
@@ -40,14 +40,14 @@ echo "## Setting up testApp..."
 cd ../testApp
 autoreconf -i
 ./configure --prefix=$PWD/../..
-make install -j
+make install -j8
 cd ../..
 
 echo "## Setting up LightComm..."
 cd LightCommSimulator
 autoreconf -i
 ./configure --prefix=$PWD/..
-make install -j
+make install -j8
 cd ..
 
 export PATH=$PWD/bin:$PATH
