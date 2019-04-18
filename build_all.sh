b471c653cfd86ca7ceb45c90a831c9e3ba395d0b
leo <luecken@math.hu-berlin.de>
2019-04-18 14:03:02 +0200

    Modified build script: enable c++11 for ns3 and ensure that baseApp is built before other apps, refs #48
    Former-commit-id: ace99948618b17d3fd77934633bd6996661fc36c

--- build_all.sh
+++ build_all.sh
@@ -35,7 +35,7 @@ cd ../../..
 
 cd ns-3.20
 echo "## Building ns-3 in $PWD..."
-./waf configure $waf_opt --prefix=$PWD/..
+CXXFLAGS="-std=c++11" ./waf configure $waf_opt --prefix=$PWD/..
 if ! ./waf -j8; then
     echo "### Build failed!"
     cd ..
@@ -56,6 +56,18 @@ fi
 cd ..
 
 cd iTETRIS-Applications
+
+cd baseApp
+echo "## Building baseApp in $PWD..."
+autoreconf -i
+./configure $config_opt --prefix=$PWD/../..
+if ! make install -j8; then
+    echo "### Build failed!"
+    cd ../..
+    exit 1
+fi
+cd ..
+
 for app in *; do
     cd $app
     if test -e configure.ac; then
