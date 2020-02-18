7fca59e8a2d49d70ce7c263d50a1933287fe8226
Matthias Schwamborn <matthias.schwamborn@dlr.de>
2020-02-18 16:20:28 +0100

    extract BaseVehicle class from UC5 with extrapolation of CAM-based info; refs #150 #154
    Former-commit-id: fb2e7b5f51444f49e252fc5a5c40b4a75b7ca1f9

---
+++ iTETRIS-Applications/baseApp/src/application/model/BaseVehicle.cpp
@@ -0,0 +1,142 @@
+/*
+ * BaseVehicle.cpp
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: leo, matthias
+ */
+
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include "BaseVehicle.h"
+#include "application/model/headers.h"
+
+namespace baseapp {
+namespace application {
+
+BaseVehicle::BaseVehicle()
+    : xPosition(-1),
+      speed(-1),
+      accel(0),
+      xPosition_received(xPosition),
+      speed_received(speed),
+      accel_received(accel),
+      laneIndex(-1),
+      length(-1),
+      minGap(-1),
+      sumoID(""),
+      icsID(-1),
+      automationType(AT_UNKNOWN),
+      lastUpdateTime(-1),
+      lastExtrapolationTime(-1),
+      initialInformationReceived(false) {}
+
+BaseVehicle::BaseVehicle(double pos, double speed, double accel, int laneIndex,
+                         double length, double minGap, std::string sumoID,
+                         int icsID, AutomationType automationType)
+    : xPosition(pos),
+      speed(speed),
+      accel(accel),
+      xPosition_received(xPosition),
+      speed_received(speed),
+      accel_received(accel),
+      laneIndex(laneIndex),
+      length(length),
+      minGap(minGap),
+      sumoID(sumoID),
+      icsID(icsID),
+      automationType(automationType),
+      lastUpdateTime(-1),
+      lastExtrapolationTime(-1),
+      initialInformationReceived(true) {}
+
+BaseVehicle::BaseVehicle(std::string sumoID, int icsID)
+    : xPosition(-1),
+      speed(-1),
+      accel(0),
+      xPosition_received(xPosition),
+      speed_received(speed),
+      accel_received(accel),
+      laneIndex(-1),
+      length(-1),
+      minGap(-1),
+      sumoID(sumoID),
+      icsID(icsID),
+      automationType(AT_UNKNOWN),
+      lastUpdateTime(-1),
+      lastExtrapolationTime(-1),
+      initialInformationReceived(false) {}
+
+double
+BaseVehicle::getSpacing(const std::shared_ptr<BaseVehicle> leader) const {
+  return leader->xPosition - leader->length - minGap - xPosition;
+}
+
+void
+BaseVehicle::updateState(int currentTime, const baseapp::application::TransaidHeader::CamInfo* info) {
+    if (lastUpdateTime > info->generationTime) {  // this CAM is outdated
+        if (DEBUG_BASEVEHICLE) {
+            std::cout << currentTime << " veh '" << sumoID
+                      << "' received outdated CAM with generationTime "
+                      << info->generationTime
+                      << " (lastUpdateTime = " << lastUpdateTime << ")\n"
+                      << std::endl;
+        }
+        return;
+    }
+
+    lastUpdateTime = info->generationTime;
+    accel_received = info->acceleration;
+    speed_received = info->speed;
+    xPosition_received = info->position.x;
+    laneIndex = info->laneIndex;
+
+    // invalidate current estimation
+    lastExtrapolationTime = -1;
+
+    // Extrapolate received info to current state
+    extrapolateState(currentTime);
+}
+
+void
+BaseVehicle::extrapolateState(int currentTime) {
+    if (currentTime == lastExtrapolationTime) {
+        return;
+    }
+
+    if (DEBUG_BASEVEHICLE) {
+        std::cout << currentTime << " extrapolateState() for veh '" << sumoID << "'\n"
+                << "   xPos = " << xPosition << ", speed = " << speed << ", accel = " << accel << ","
+                << "   lastUpdate = " << lastUpdateTime << ", lastExtrapolation = " << lastExtrapolationTime
+                << std::endl;
+    }
+
+    double dt, x0, v0, a0;
+    if (lastExtrapolationTime < 0) {
+        // don't rely on the current state for extrapolation
+        dt = (currentTime - lastUpdateTime) * 0.001;
+        x0 = xPosition = xPosition_received;
+        v0 = speed = speed_received;
+        a0 = accel = accel_received;
+    } else {
+        // continue extrapolation using the current state
+        dt = (currentTime - lastExtrapolationTime) * 0.001;
+        x0 = xPosition;
+        v0 = speed;
+        a0 = accel;
+    }
+
+    // Do the extrapolation assuming damped acceleration
+    accel = a0 * pow(ACCEL_ESTIMATION_DAMPING, dt);
+    speed += (accel + a0) * 0.5 * dt;
+    xPosition += (speed + v0) * 0.5 * dt;
+
+    if (DEBUG_BASEVEHICLE) {
+        std::cout << "Extrapolated:\n" << " xPos = " << xPosition << ", speed = " << speed << ", accel = " << accel << std::endl;
+    }
+
+    lastExtrapolationTime = currentTime;
+}
+
+} /* namespace application */
+} /* namespace baseapp */
---
+++ iTETRIS-Applications/baseApp/src/application/model/BaseVehicle.h
@@ -0,0 +1,106 @@
+/*
+ * BaseVehicle.h
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: leo, matthias
+ */
+
+#ifndef SRC_APPLICATION_MODEL_BASEVEHICLE_H_
+#define SRC_APPLICATION_MODEL_BASEVEHICLE_H_
+
+#include <map>
+#include <set>
+#include <vector>
+#include <memory>
+#include "application/model/headers.h"
+
+#define DEBUG_BASEVEHICLE false
+
+/// Parameters for handling uncertainties in vehicle states
+// Factor by which the extrapolated acceleration for CAVs since their last V2X
+// state update is damped
+// @see BaseVehicle::extrapolateState()
+#define ACCEL_ESTIMATION_DAMPING 0.75
+
+namespace baseapp {
+namespace application {
+
+enum AutomationType {
+    AT_UNKNOWN = 0,
+    AT_MANUAL,
+    AT_CV,
+    AT_CAV,
+    AT_TOR_ISSUED
+};
+
+/// @brief map: vType identifier -> AutomationType
+static std::map<std::string, AutomationType> vTypeAutomationTypeMap = {
+    {"CAV", AT_CAV},
+    {"CAVGA", AT_CAV},
+    {"CAVGB", AT_CAV},
+    {"CV", AT_CV},
+    {"LV", AT_MANUAL},
+    {"HGV", AT_MANUAL},
+    {"LGV", AT_MANUAL}
+};
+
+/// @brief Available ToC lead times for the different vehicle types
+/// map: identifier -> double
+static std::map<AutomationType, double> ToC_lead_times = {
+    {AT_CAV, 10.0},
+    {AT_CV, 1.0},
+    {AT_MANUAL, -1.0}
+};
+
+class BaseVehicle {
+public:
+
+    BaseVehicle();
+    BaseVehicle(double pos, double speed, double accel, int laneIndex,
+                double length, double minGap, std::string sumoID, int icsID,
+                AutomationType automationType);
+    BaseVehicle(std::string sumoID, int icsID);
+    virtual ~BaseVehicle(){};
+
+    // Returns the positive front to backbumper distance between this and the
+    // given leader if applicable, -1 otherwise.
+    // @note: this assumes that both vehicles are on the same edge to avoid
+    // complicated calculations or a delayed result by waiting for response
+    // from a traci.getDrivingDistance() call.
+    double getSpacing(const std::shared_ptr<BaseVehicle> leader) const;
+
+    /// @brief Update the vehicle state according to the received CAM info
+    void updateState(int currentTime, const baseapp::application::TransaidHeader::CamInfo* info);
+
+    /// @brief Estimates the current state at time t based on the last received CAM
+    /// @note Uses a very rough estimation assuming exponentially damped
+    /// acceleration since the last V2X reception
+    /// @see ACCEL_ESTIMATION_DAMPING
+    void extrapolateState(int currentTime);
+
+    // State variables that represent the current estimated state
+    double xPosition;
+    double speed;
+    double accel;
+    int lastExtrapolationTime;
+    // State variables retrieved from a V2X message at @lastUpdateTime
+    double xPosition_received;
+    double speed_received;
+    double accel_received;
+    int lastUpdateTime;
+    // automation related state flag
+    AutomationType automationType;
+    // "Static" vehicle properties
+    int laneIndex;
+    double minGap;
+    double length;
+    std::string sumoID;
+    int icsID;
+    // whether the vehicles static properties are initialized
+    bool initialInformationReceived;
+};
+
+} /* namespace application */
+} /* namespace baseapp */
+
+#endif /* SRC_APPLICATION_MODEL_BASEVEHICLE_H_ */
